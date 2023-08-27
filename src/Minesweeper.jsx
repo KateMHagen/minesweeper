0e2c34ab37632d9921147636e4cc0fb50cafccfa
Kate <katemhagen@gmail.com>
2023-08-26 19:13:56 -0700

    done with most logic

--- src/Minesweeper.jsx
+++ src/Minesweeper.jsx
@@ -3,6 +3,12 @@ import { useState, useEffect } from "react"
 const numRows = 8
 const numCols = 8
 const numMines = 10
+// Directions you can go to get to neighbouring cells
+let directions = [
+    [-1,-1], [-1,0], [-1,1],
+    [0,-1], [0,1],
+    [1,-1], [1,0], [1,1]
+]
 
 function generateEmptyBoard() {
     const board = []
@@ -18,7 +24,6 @@ function generateEmptyBoard() {
         }
         board.push(row)
     }
-  
     return board
 }
 
@@ -37,10 +42,25 @@ function placeMines(board) {
     }
 }
 
-
-function countAdjacentMines(board, row, col) {
-
+function countAdjacentMines(board, rowIndex, colIndex) {
+    // Keep a count of the adjacent mines
+    let count = 0
+ 
+    for(const [x,y] of directions) {
+        let newRow = rowIndex + x
+        let newCol = colIndex + y
+        
+        // Check if neighbor cells are in bounds
+        if(newRow >= 0 && newRow < numRows && newCol >= 0 && newCol < numCols) {
+            // Check if neighbor cell isMine
+            if(board[newRow][newCol].isMine) {
+                count++
+            }
+        }
+    }
+    return count
 }
+
 function gameOver(){
     console.log("Game over")
 }
@@ -52,37 +72,46 @@ export default function Minesweeper() {
     useEffect(() => {
         const newBoard = generateEmptyBoard()
         placeMines(newBoard)
-        // countAdjacentMines(newBoard)
+        for(let rows = 0; rows < numRows; rows++) {
+            for(let cols = 0; cols < numCols; cols++) {
+                newBoard[rows][cols].adjacentMines = countAdjacentMines(newBoard, rows, cols)
+            }
+        }
         setBoard(newBoard)
     }, [])
 
-    function revealCell(board, rowIndex,colIndex){
-        // Check if row or col index is out of bounds
-        if (!(rowIndex < 0 || rowIndex >= numRows || colIndex < 0 || colIndex >= numCols)) {
-    
-            const cell = board[rowIndex][colIndex]
-
-            if(cell.isMine) {
-                gameOver()
-            }
-
-            if(!cell.isRevealed) {
-                const copy = [...board]
-
-                // If the cell has adjacent mines, only reveal that cell
-                if(cell.adjacentMines > 0) {
-                    copy[rowIndex][colIndex].isRevealed = true
-                    return setBoard(copy)
-                }
-
-                // If cell has no adjacent mines, reveal it and recursively reveal neighbours
-                //
-                //
-                //
-            }
-
+    function revealCell(board, rowIndex,colIndex) {
+        // Check if row or col index is out bounds
+        if (rowIndex < 0 || rowIndex >= numRows || colIndex < 0 || colIndex >= numCols) {
+            return board;
         }
-        return board
+
+        let copy = [...board]
+        let cell = copy[rowIndex][colIndex]
+
+        if(cell.isMine) {
+            cell.isRevealed = true
+            setBoard(copy)
+            gameOver()
+            return board
+        }
+            
+        if(!cell.isRevealed) {
+            // If the cell has adjacent mines, only reveal that cell
+            if(cell.adjacentMines > 0) {
+                cell.isRevealed = true
+                return setBoard(copy)
+            } 
+
+            // If cell has no adjacent mines, reveal it and recursively reveal neighbours
+            cell.isRevealed = true
+            setBoard(copy)
+            for(const [x, y] of directions) {
+                cell.isRevealed = true
+                console.log(copy)
+                revealCell(copy,rowIndex + x, colIndex + y)
+            }
+        }
     }
     
     return (
@@ -92,9 +121,10 @@ export default function Minesweeper() {
                     {row.map((col, colIndex) => (
                         <div 
                             key={colIndex} 
-                            className={`cell ${board[rowIndex][colIndex].isRevealed ? "revealed" : "notRevealed"}`}
                             // Reveal cell if clicked
                             onClick={() => revealCell(board, rowIndex, colIndex)}
+                            className={`cell ${board[rowIndex][colIndex].isRevealed ? "revealed" : "notRevealed"}`}
+                            
                             // Flag cell if right clicked
                             //
                             //
